ee0ae7783e00c4b6dd05f430e638bb4a94c531db
umibozu <umibozu@0cdd8415-4f48-0410-be57-ea0a3743afa2>
2010-04-10 13:17:50 +0000

    GPL2 -> GPL3

--- trunk/MineSweeper/src/net/eiroca/j2me/minesweeper/CustomLevelForm.java
+++ trunk/MineSweeper/src/net/eiroca/j2me/minesweeper/CustomLevelForm.java
@@ -1,11 +1,10 @@
-/** GPL >= 2.0
- * Based upon J2ME Minesweeper.
+/** GPL >= 3.0
+ * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  * Copyright (C) M. Jumari
- * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,9 +12,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
  */
 package net.eiroca.j2me.minesweeper;
 
@@ -24,12 +22,23 @@ import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.TextField;
 import net.eiroca.j2me.app.Application;
 
+/**
+ * The Class CustomLevelForm.
+ */
 public class CustomLevelForm extends Form {
 
+  /** The t height. */
   private final TextField tHeight;
+
+  /** The t width. */
   private final TextField tWidth;
+
+  /** The t bomb. */
   private final TextField tBomb;
 
+  /**
+   * Instantiates a new custom level form.
+   */
   public CustomLevelForm() {
     super(Application.messages[MineSweeper.MSG_CUSTOMLEVEL]);
     tHeight = new TextField(Application.messages[MineSweeper.MSG_CL_HEIGTH], "", 2, TextField.NUMERIC);
@@ -40,12 +49,20 @@ public class CustomLevelForm extends Form {
     append(tBomb);
   }
 
+  /**
+   * Sets the inputs.
+   */
   public void setInputs() {
     tHeight.setString(Integer.toString(MineSweeper.height));
     tWidth.setString(Integer.toString(MineSweeper.width));
     tBomb.setString(Integer.toString(MineSweeper.bomb));
   }
 
+  /**
+   * Gets the inputs.
+   * 
+   * @return the inputs
+   */
   public void getInputs() {
     MineSweeper.height = Integer.parseInt(tHeight.getString());
     MineSweeper.width = Integer.parseInt(tWidth.getString());
--- trunk/MineSweeper/src/net/eiroca/j2me/minesweeper/game/MineSweeperGame.java
+++ trunk/MineSweeper/src/net/eiroca/j2me/minesweeper/game/MineSweeperGame.java
@@ -1,11 +1,10 @@
-/** GPL >= 2.0
- * Based upon J2ME Minesweeper.
+/** GPL >= 3.0
+ * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  * Copyright (C) M. Jumari
- * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -13,41 +12,75 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
  */
 package net.eiroca.j2me.minesweeper.game;
 
 import java.util.Vector;
 import net.eiroca.j2me.app.BaseApp;
 
+/**
+ * The Class MineSweeperGame.
+ */
 public class MineSweeperGame {
 
+  /** The Constant GE_RUNNING. */
   public static final int GE_RUNNING = 0;
+
+  /** The Constant GE_EXPLODED. */
   public static final int GE_EXPLODED = 1;
+
+  /** The Constant GE_RESOLVED. */
   public static final int GE_RESOLVED = 2;
 
+  /** The Constant MINE_MIN_SIZE. */
   public static final int MINE_MIN_SIZE = 9;
+
+  /** The Constant MINE_MAX_SIZE. */
   public static final int MINE_MAX_SIZE = 30;
+
+  /** The Constant MINE_CHECKED. */
   public static final int MINE_CHECKED = -100;
+
+  /** The Constant MINE_UNCHECKED. */
   public static final int MINE_UNCHECKED = -200;
+
+  /** The Constant MINE_BOMB. */
   public static final int MINE_BOMB = -300;
+
+  /** The Constant MIN_BOMB. */
   public static final int MIN_BOMB = 9;
+
+  /** The Constant MAX_BOMB. */
   public static final int MAX_BOMB = 99;
 
+  /** The size_width. */
   public int size_width = 9;
+
+  /** The size_height. */
   public int size_height = 9;
+
+  /** The bomb. */
   public int bomb = 10;
+
+  /** The status. */
   public int status = MineSweeperGame.GE_RUNNING;
+
+  /** The field. */
   public MineInfo[][] field;
 
+  /**
+   * Instantiates a new mine sweeper game.
+   */
   public MineSweeperGame() {
     //
   }
 
   /**
-   * Fungsi untuk membuat game baru dengan paramater Level B = Beginer, I = Intermediate, E = Expert
+   * Fungsi untuk membuat game baru dengan paramater Level B = Beginer, I = Intermediate, E = Expert.
+   * 
+   * @param level the level
    */
   public void newGame(final char level) {
     if (level == 'B') {
@@ -62,7 +95,8 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk membuat game baru dengan parameter lebar, tinggi dan jumlah bomb
+   * Fungsi untuk membuat game baru dengan parameter lebar, tinggi dan jumlah bomb.
+   * 
    * @param width lebar game
    * @param height tinggi game
    * @param bomb jumlah bomb
@@ -76,9 +110,11 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungi untuk klik kiri game
+   * Fungi untuk klik kiri game.
+   * 
    * @param x koordinat x
    * @param y koordinat y
+   * @return the vector
    */
   public Vector checkCell(final int x, final int y) {
     final Vector result = new Vector();
@@ -102,9 +138,11 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungi untuk klik kanan game
+   * Fungi untuk klik kanan game.
+   * 
    * @param x koordinat x
    * @param y koordinat y
+   * @return the vector
    */
   public Vector markBomb(final int x, final int y) {
     final Vector result = new Vector();
@@ -120,6 +158,12 @@ public class MineSweeperGame {
     return result;
   }
 
+  /**
+   * Double check.
+   * 
+   * @param result the result
+   * @param mi the mi
+   */
   public void doubleCheck(final Vector result, final MineInfo mi) {
     if ((mi.status_guess == MineSweeperGame.MINE_UNCHECKED) && (mi.status_real == MineSweeperGame.MINE_BOMB)) {
       showBomb(result);
@@ -133,6 +177,13 @@ public class MineSweeperGame {
     }
   }
 
+  /**
+   * Can double click.
+   * 
+   * @param x the x
+   * @param y the y
+   * @return true, if successful
+   */
   public boolean canDoubleClick(final int x, final int y) {
     final MineInfo m = field[x][y];
     boolean result = false;
@@ -160,9 +211,11 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungi untuk klik ganda game
+   * Fungi untuk klik ganda game.
+   * 
    * @param x koordinat x
    * @param y koordinat y
+   * @return the vector
    */
   public Vector doubleClick(final int x, final int y) {
     final Vector result = new Vector();
@@ -191,7 +244,8 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk mencari jumlah kotak yang ditandai
+   * Fungsi untuk mencari jumlah kotak yang ditandai.
+   * 
    * @return jumlah kotak yang ditandai dan belum terbuka
    */
   public int checked() {
@@ -207,7 +261,8 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk mencari jumlah kotak yang belum ditandai
+   * Fungsi untuk mencari jumlah kotak yang belum ditandai.
+   * 
    * @return jumlah kotak yang belum ditandai dan belum terbuka
    */
   private int unchecked() {
@@ -223,7 +278,7 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk mengecek menang
+   * Fungsi untuk mengecek menang.
    */
   private void checkVictory() {
     if (checked() + unchecked() == bomb) {
@@ -231,6 +286,12 @@ public class MineSweeperGame {
     }
   }
 
+  /**
+   * Check.
+   * 
+   * @param vector the vector
+   * @param mi the mi
+   */
   private void check(final Vector vector, final MineInfo mi) {
     if (!vector.contains(mi)) {
       if ((mi.status_guess == MineSweeperGame.MINE_UNCHECKED) && (mi.status_real >= 0)) {
@@ -241,7 +302,8 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk membuka sekitar
+   * Fungsi untuk membuka sekitar.
+   * 
    * @param res vector temporery yang digunakan
    */
   private void checkMines(final Vector res) {
@@ -272,7 +334,9 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk membuka semua bomb
+   * Fungsi untuk membuka semua bomb.
+   * 
+   * @param result the result
    */
   private void showBomb(final Vector result) {
     status = MineSweeperGame.GE_EXPLODED;
@@ -288,7 +352,7 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk membuat kotak game baru
+   * Fungsi untuk membuat kotak game baru.
    */
   private void init() {
     // buat kotak kosong
@@ -321,7 +385,8 @@ public class MineSweeperGame {
   }
 
   /**
-   * Fungsi untuk mencari jumlah bomb disekitar kotak
+   * Fungsi untuk mencari jumlah bomb disekitar kotak.
+   * 
    * @param x koordinat x
    * @param y koordinat y
    * @return jumlah kotak yang ditandai sebaga bomb yang ada disekitar kotak dan belum terbuka
