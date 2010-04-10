d836c1d3f835913fe46e69a582b2973697aa04f9
umibozu <umibozu@0cdd8415-4f48-0410-be57-ea0a3743afa2>
2010-04-10 13:25:29 +0000

    GPL2 -> GPL3

--- trunk/libJ2ME_GameSkel/src/EGame.java
+++ trunk/libJ2ME_GameSkel/src/EGame.java
@@ -1,16 +1,31 @@
-/**
- * (C) 2006 eIrOcA
+/** GPL >= 3.0
+ * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
  */
 import net.eiroca.j2me.app.Application;
 import net.eiroca.j2me.game.GameApp;
 import net.eiroca.j2me.game.GameScreen;
 
+/**
+ * The Class EGame.
+ */
 public class EGame extends GameApp {
 
+  /**
+   * Instantiates a new e game.
+   */
   public EGame() {
     super();
     Application.menu = new short[][] {
@@ -35,10 +50,16 @@ public class EGame extends GameApp {
 
   }
 
+  /* (non-Javadoc)
+   * @see net.eiroca.j2me.game.GameApp#getGameScreen()
+   */
   public GameScreen getGameScreen() {
     return new EGameScreen(this, true, true);
   }
 
+  /* (non-Javadoc)
+   * @see net.eiroca.j2me.game.GameApp#processGameAction(int)
+   */
   public void processGameAction(final int action) {
     switch (action) {
       case GA_STARTUP: // Continue
--- trunk/libJ2ME_GameSkel/src/EGameScreen.java
+++ trunk/libJ2ME_GameSkel/src/EGameScreen.java
@@ -1,27 +1,52 @@
-/**
- * (C) 2006 eIrOcA
+/** GPL >= 3.0
+ * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
- * This program is free software; you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
  */
 import javax.microedition.lcdui.game.GameCanvas;
 import net.eiroca.j2me.app.Application;
 import net.eiroca.j2me.game.GameApp;
 import net.eiroca.j2me.game.GameScreen;
 
+/**
+ * The Class EGameScreen.
+ */
 public final class EGameScreen extends GameScreen {
 
+  /**
+   * Instantiates a new e game screen.
+   * 
+   * @param midlet the midlet
+   * @param suppressKeys the suppress keys
+   * @param fullScreen the full screen
+   */
   public EGameScreen(final GameApp midlet, final boolean suppressKeys, final boolean fullScreen) {
     super(midlet, suppressKeys, fullScreen);
     name = "EGame";
   }
 
+  /* (non-Javadoc)
+   * @see net.eiroca.j2me.game.GameScreen#init()
+   */
   public void init() {
     super.init();
     score.beginGame(1, 0, 0);
   }
 
+  /* (non-Javadoc)
+   * @see net.eiroca.j2me.game.GameScreen#tick()
+   */
   public boolean tick() {
     score.addScore(1);
     final int keyStates = getKeyStates();
@@ -35,6 +60,9 @@ public final class EGameScreen extends GameScreen {
     return true;
   }
 
+  /**
+   * Draw.
+   */
   public void draw() {
     // clear screen to black
     screen.setColor(Application.background);
