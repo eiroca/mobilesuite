971f1b8ede743ed270193055a66365c20f0b4f14
umibozu <umibozu@0cdd8415-4f48-0410-be57-ea0a3743afa2>
2010-04-10 13:24:31 +0000

    GPL2 -> GPL3

--- TestSuite_Tools/src/BuildPackageTree.java
+++ TestSuite_Tools/src/BuildPackageTree.java
@@ -1,7 +1,22 @@
-/** MIT LICENSE
+/** GPL >= 3.0 + MIT
  * Based upon Mobile Device Tools written by Andrew Scott
+ * 
+ * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
+ * 
  * Copyright (C) 2004 Andrew Scott
- * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- TestSuite_Tools/src/net/eiroca/j2me/TestSuite/ProcessorResult.java
+++ TestSuite_Tools/src/net/eiroca/j2me/TestSuite/ProcessorResult.java
@@ -1,10 +1,9 @@
-/** GPL >= 2.0
+/** GPL >= 3.0
+ * Copyright (C) 2006-2010 eIrOcA (eNrIcO Croce & sImOnA Burzio)
  *
- * Copyright (C) 2006-2008 eIrOcA (eNrIcO Croce & sImOnA Burzio)
- *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -12,9 +11,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/
  */
 package net.eiroca.j2me.TestSuite;
 
@@ -30,7 +28,10 @@ import java.util.Vector;
  */
 public class ProcessorResult extends Processor {
 
+  /** The NOWIK i1. */
   private static String NOWIKI1 = "<nowiki>";
+  
+  /** The NOWIK i2. */
   private static String NOWIKI2 = "</nowiki>";
 
   /* (non-Javadoc)
